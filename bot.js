b5aa5780b0edc2b5bfdf98bdc097bb0aa09bb111
ahmad123123123sad <45146326+ahmad123123123sad@users.noreply.github.com>
2018-11-23 12:16:56 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,10 +2,10 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
  
 client.on('ready', async() => {
-var server = "515234485609168903"; 
-var channel = "515237042729385988";
+var server = "515469920784482306"; 
+var channel = "515469982008868865";
     setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send(' ريما :heart_eyes: ريما :heart_eyes: ريما :heart_eyes: ريما :heart_eyes: ريما :heart_eyes: ريما :heart_eyes: ريما :heart_eyes: ريما :heart_eyes: ريما :heart_eyes: ريما :heart_eyes: ريما :heart_eyes: ريما :heart_eyes: ')
+    client.guilds.get(server).channels.get(channel).send('** ريما :heart_eyes: ريما :heart_eyes: ريما :heart_eyes: ريما :heart_eyes: ريما :heart_eyes: ريما :heart_eyes: ريما :heart_eyes: ريما :heart_eyes: ريما :heart_eyes: ريما :heart_eyes: ريما :heart_eyes: ريما :heart_eyes: **')
     },305);
 })
 
